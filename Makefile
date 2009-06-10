95427c066327dc4d443e727a27f3d7f4834270c4
Felipe Contreras <felipe.contreras@gmail.com>
2009-06-10 22:35:33 +0300

    Add async queue
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ dspdir := /lib/dsp
 gst_plugin := libgstdsp.so
 
 $(gst_plugin): plugin.o gstdspdummy.o gstdspbase.o gstdspvdec.o \
-	gstdspvenc.o gstdsph263enc.o gstdspjpegenc.o dsp_bridge.o util.o log.o gstdspparse.o
+	gstdspvenc.o gstdsph263enc.o gstdspjpegenc.o dsp_bridge.o util.o log.o gstdspparse.o async_queue.o
 $(gst_plugin): CFLAGS := $(CFLAGS) $(UTIL_CFLAGS) $(GST_CFLAGS) -D VERSION='"$(version)"' -D DSPDIR='"$(dspdir)"'
 $(gst_plugin): LIBS := $(UTIL_LIBS) $(GST_LIBS)
 
---
+++ async_queue.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2008-2009 Nokia Corporation.
+ *
+ * Author: Felipe Contreras <felipe.contreras@nokia.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation
+ * version 2.1 of the License.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ */
+
+#ifndef ASYNC_QUEUE_H
+#define ASYNC_QUEUE_H
+
+#include <glib.h>
+
+typedef struct AsyncQueue AsyncQueue;
+
+struct AsyncQueue
+{
+	GMutex *mutex;
+	GCond *condition;
+	GList *head;
+	GList *tail;
+	guint length;
+	gboolean enabled;
+};
+
+AsyncQueue *async_queue_new(void);
+void async_queue_free(AsyncQueue *queue);
+void async_queue_push(AsyncQueue *queue, gpointer data);
+gpointer async_queue_pop(AsyncQueue *queue);
+gpointer async_queue_pop_forced(AsyncQueue *queue);
+void async_queue_disable(AsyncQueue *queue);
+void async_queue_enable(AsyncQueue *queue);
+void async_queue_flush(AsyncQueue *queue);
+
+#endif /* ASYNC_QUEUE_H */
