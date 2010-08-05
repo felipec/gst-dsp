2b0caaf22ca79c362dd0f6230e5081cbae9eb5b6
Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
2010-08-05 15:06:26 +0200

    base: do not consume buffer timestamp for a 0-length output buffer
    SN might return 0-length buffer typically as output buffer corresponding
    to a codec-data input buffer (or alike).  In any case, it does not represent
    real frame data and as such should not consume a real buffer's timestamp.
    Moreover, if this occurs very early on as part of codec-data handling,
    then there may not yet be any input timestamps stored (so count == 0),
    which would temporarily then lead to count == -1.
    On the other hand, if a real ts is consumed this way, following buffers will
    have wrong timestamps, most notable following a seek.
    
    Signed-off-by: Mark Nauwelaerts <mark.nauwelaerts@collabora.co.uk>
    Signed-off-by: Felipe Contreras <felipe.contreras@nokia.com>

--- gstdspbase.c
+++ gstdspbase.c
@@ -378,13 +378,7 @@ output_loop(gpointer data)
 	if (G_UNLIKELY(!b->len)) {
 		/* no need to process this buffer */
 		pr_warning(self, "empty buffer");
-		g_mutex_lock(self->ts_mutex);
-		flush_buffer = (self->ts_out_pos != self->ts_push_pos);
-		self->ts_out_pos = (self->ts_out_pos + 1) % ARRAY_SIZE(self->ts_array);
-		self->ts_count--;
-		if (G_LIKELY(!flush_buffer))
-			self->ts_push_pos = self->ts_out_pos;
-		g_mutex_unlock(self->ts_mutex);
+		/* no real frame data, so no need to consume a real frame's ts */
 		goto leave;
 	}
 
