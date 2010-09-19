581f32e52df7bf7b91d2dfd2d2348b81c172cda3
Felipe Contreras <felipe.contreras@gmail.com>
2010-09-19 16:56:27 +0300

    dummy: improve mapping
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- gstdspdummy.c
+++ gstdspdummy.c
@@ -268,33 +268,35 @@ change_state(GstElement *element,
 	return ret;
 }
 
+static inline bool
+buffer_is_aligned(GstBuffer *buf, dmm_buffer_t *b)
+{
+	if ((unsigned long) GST_BUFFER_DATA(buf) % b->alignment != 0)
+		return false;
+	if (((unsigned long) GST_BUFFER_DATA(buf) + GST_BUFFER_SIZE(buf)) % b->alignment != 0)
+		return false;
+	return true;
+}
+
 static inline void
 map_buffer(GstDspDummy *self,
 	   GstBuffer *g_buf,
 	   dmm_buffer_t *d_buf)
 {
-#if 0
-	if (d_buf->alignment == 0 ||
-	    (unsigned long) GST_BUFFER_DATA(g_buf) % d_buf->alignment == 0)
-	{
-		if (d_buf->data != GST_BUFFER_DATA(g_buf))
-			dmm_buffer_use(d_buf, GST_BUFFER_DATA(g_buf), GST_BUFFER_SIZE(g_buf));
+	if (d_buf->alignment == 0 || buffer_is_aligned(g_buf, d_buf)) {
+		dmm_buffer_use(d_buf, GST_BUFFER_DATA(g_buf), GST_BUFFER_SIZE(g_buf));
+		gst_buffer_ref(g_buf);
 		d_buf->user_data = g_buf;
 		return;
 	}
 
 	if (d_buf->alignment != 0) {
-		GST_WARNING("buffer not aligned: %p, %lu",
-			    GST_BUFFER_DATA(g_buf),
-			    (unsigned long) GST_BUFFER_DATA(g_buf) % d_buf->alignment);
+		pr_warning(self, "buffer not aligned: %p-%p",
+			   GST_BUFFER_DATA(g_buf),
+			   GST_BUFFER_DATA(g_buf) + GST_BUFFER_SIZE(g_buf));
 	}
-#endif
 
-	/* reallocate? */
-	if (!d_buf->allocated_data ||
-	    d_buf->size > GST_BUFFER_SIZE(g_buf)) {
-		dmm_buffer_allocate(d_buf, GST_BUFFER_SIZE(g_buf));
-	}
+	dmm_buffer_allocate(d_buf, GST_BUFFER_SIZE(g_buf));
 	d_buf->need_copy = true;
 }
 
